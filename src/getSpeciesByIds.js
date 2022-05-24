36066e6b8370b1671753d444b746833fe2cfe1ef
Solon <amaral.solon@hotmail.com>
2022-05-24 17:17:52 -0300

    Requisito 1 :checkered_flag:

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const { species } = data;
+
+const getSpeciesByIds = (...ids) => species.filter((id) => ids.includes(id.id));
+
+console.log(getSpeciesByIds('ef3778eb-2844-4c7c-b66c-f432073e1c6b', '01422318-ca2d-46b8-b66c-3e9e188244ed', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 
 module.exports = getSpeciesByIds;
