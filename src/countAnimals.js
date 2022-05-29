e536fa7a24730ec3ebb3d25d34ba6a94c89c5c42
Solon <amaral.solon@hotmail.com>
2022-05-28 23:11:16 -0300

    Requisito 5 :checkered_flag:

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const object = {};
+    species.forEach((specie) => {
+      object[specie.name] = specie.residents.length;
+    });
+    return object;
+  }
+  if (typeof animal === 'object' && animal.sex === undefined) {
+    return species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+  return species.find((specie) => specie.name === animal.specie)
+    .residents.filter((resident) => resident.sex === animal.sex).length;
 }
 
+console.log(countAnimals({ specie: 'giraffes' }));
 module.exports = countAnimals;
