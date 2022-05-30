3c9636cfcba11fa848870f176e3f749f4790075a
Solon <amaral.solon@hotmail.com>
2022-05-29 23:42:42 -0300

    Requisito 8 :checkered_flag:

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
+
+function countEntrantsReduce(entrants) {
+  const result = entrants.reduce((acc, cur) => {
+    if (cur.age < 18) {
+      acc.child += 1;
+    }
+    if (cur.age > 17 && cur.age < 50) {
+      acc.adult += 1;
+    }
+    if (cur.age >= 50) {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
+  return result;
+}
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (!entrants || !entrants[0]) {
+    return 0;
+  }
+  return countEntrantsReduce(entrants);
 }
+console.log(countEntrants({}));
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || entrants === undefined || !entrants[0]) {
+    return 0;
+  }
+  return countEntrants(entrants).child * prices.child
+    + countEntrants(entrants).adult * prices.adult
+    + countEntrants(entrants).senior * prices.senior;
 }
-
+console.log(calculateEntry({}));
 module.exports = { calculateEntry, countEntrants };
