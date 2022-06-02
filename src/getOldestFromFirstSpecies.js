231d34c32efd55fed468e64ccce0c3a27342041f
Solon <amaral.solon@hotmail.com>
2022-06-01 22:42:23 -0300

    Requisito 10 (Refatorado) :checkered_flag:

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,28 +1,14 @@
 const data = require('../data/zoo_data');
 const { employees, species } = require('../data/zoo_data');
 
-const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
-const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
-const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
-
-function getEmployeeById(employeeId) {
-  if (!employeeId) {
+function getOldestFromFirstSpecies(id) {
+  if (!id) {
     return console.log('Id Inválido');
   }
-  const employ = employees.find((e) => e.id === employeeId);
-  return employ;
+  const responsableFirst = employees.find((e) => e.id === id).responsibleFor[0];
+  const residentes = species.find((especie) => especie.id === responsableFirst).residents
+    .sort((a, b) => b.age - a.age);
+  return Object.values(residentes[0]);
 }
-
-function getOldestFromFirstSpecies(id) {
-  const result = [];
-  const employee = getEmployeeById(id);
-  const responsable = employee.responsibleFor[0];
-  const firstEspecie = species.find((especie) => especie.id === responsable);
-  const firstEspecieResidents = firstEspecie.residents;
-  const sorte = firstEspecieResidents.sort((a, b) => b.age - a.age);
-  result.push(sorte[0].name, sorte[0].sex, sorte[0].age);
-  return result;
-}
-
-console.log(getOldestFromFirstSpecies(olaId));
+console.log(getOldestFromFirstSpecies());
 module.exports = getOldestFromFirstSpecies;
